1018a0ca792e8911f8c42c3e7296df471fb04bca
Victo Almeida <victorres.almeida@gmail.com>
2022-06-22 12:18:11 +0100

    add build.yml 3

--- bookstore01/settings.py
+++ bookstore01/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-&jx1%5j(+==$x^xg-%acd!+fl&7413-gwh5psmy3+-k!0#c++o'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-f*k@=53bc5!shef1-6w+m$-g)kspbaljz%8k4(j7iuc-u2_dyd"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
