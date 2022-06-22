f8d770c7adb51b2cb1dbb5708f5d9389924d4d0f
Victo Almeida <victorres.almeida@gmail.com>
2022-06-22 12:00:16 +0100

    add build.yml

--- product/viewset/product_viewset.py
+++ product/viewset/product_viewset.py
@@ -3,10 +3,8 @@
 # from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
-
 
 class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
