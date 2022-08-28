0e9772d53d4a8dff03ca209aafaa8afd7c41a059
Victor <victorres.almeida@gmail.com>
2022-08-28 14:56:47 +0100

    heroku

--- bookstore01/settings.py
+++ bookstore01/settings.py
@@ -97,6 +97,7 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = ['https://online-shop-api-v2.herokuapp.com/','https://*.127.0.0.1']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
