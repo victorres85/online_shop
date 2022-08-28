8bc038644f65564e8685799c533c22e3093febc1
Victor <victorres.almeida@gmail.com>
2022-08-28 15:27:47 +0100

    heroku

--- bookstore01/settings.py
+++ bookstore01/settings.py
@@ -28,7 +28,10 @@ DEBUG = True
 
 #
 
-CSRF_TRUSTED_ORIGINS = ['https://git.heroku.com/online-shop-api-v2.git','https://online-shop-api-v2.herokuapp.com/admin/','online-shop-api-v2.herokuapp.com', 'https://online-shop-api-v2.herokuapp.com/','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://git.heroku.com/online-shop-api-v2.git',
+    'https://online-shop-api-v2.herokuapp.com/admin/','online-shop-api-v2.herokuapp.com', 
+    'https://online-shop-api-v2.herokuapp.com/','https://*.127.0.0.1', 
+    'https://*.herokuapp.com/']
 
 ALLOWED_HOSTS = ['online-shop-api-v2.herokuapp.com', 'localhost', '127.0.0.1', 'online-shop-api-v2.herokuapp.com',
                  'https://online-shop-api-v2.herokuapp.com/', 'online-shop-api-v2.herokuapp.com']
