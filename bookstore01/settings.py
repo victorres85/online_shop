6ca4de79fd4b0ac43009c8c90072a21030a2b3a0
Victor <victorres.almeida@gmail.com>
2022-08-28 11:34:32 +0100

    heroku

--- bookstore01/settings.py
+++ bookstore01/settings.py
@@ -89,10 +89,10 @@ WSGI_APPLICATION = 'bookstore01.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": 'django.db.backends.postgresql_psycopg2'
-        "NAME": 'd2hbe6c0dm5o9b',
-        "USER": 'xqzzyaekvpjahy',
-        "PASSWORD": '9d2004f1c9473d8fff652b9a448ba7cffa2608aa8273fbaa0f6132ae66b236ed',
-        "HOST": 'ec2-3-223-242-224.compute-1.amazonaws.com',
+        "NAME": 'd6nv9q6onups70',
+        "USER": 'pynsotbcxdtbzu',
+        "PASSWORD": '9620659a9c5aaaf5e5d9cc7573b4d2ddc316f54c7bff34a4f08f85ddc23b1d34',
+        "HOST": 'ec2-34-234-240-121.compute-1.amazonaws.com',
         "PORT": "5432",
     }
 }
